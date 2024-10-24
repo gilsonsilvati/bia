a17e04368402ead7d9a984f93c2c632425db2c72
Gilson Silva <gilsonsilvati@gmail.com>
2024-10-23 21:00:44 -0300

    Removendo profile

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
-aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin 533267005733.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267005733.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
 docker tag bia:latest 533267005733.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
 docker push 533267005733.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
